6294ebaa2ec87860a1498d34559231300e748542
Gabriel Conceicao <gabrielconceicao.dev@gmail.com>
2020-05-31 04:19:01 -0300

    Photos Module and Photo Component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'photofy';
+
+  photos = [
+    {
+        url : 'https://images.hdqwalls.com/download/spider-man-8k-2020-p7-320x480.jpg',
+        description : 'Spider-man'
+    },
+    {
+        url : 'https://images.hdqwalls.com/download/ragnar-lothbrok-assassins-creed-valhalla-8k-s0-320x480.jpg',
+        description : 'Assassins Creed Valhalla'
+    },
+    {
+        url : 'https://images.hdqwalls.com/download/harley-quinn-latest-artwork-aj-320x480.jpg',
+        description : 'Harley Quinn'
+    }
+  ];
+
 }
