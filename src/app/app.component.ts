71dd8c0ef78a8fea045726ae31fc82f4bee5f0a6
Gabriel Conceicao <gabrielconceicao.dev@gmail.com>
2020-05-31 05:59:25 -0300

    Improving the application code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos: Response[] = [];
+  photos: Photo[] = [];
 
-  constructor(photoService: PhotoService) {
-      photoService
-        .listByUser('flavio')
-        .subscribe(
-          photos => this.photos = photos,
-          err => {throw new Error(err.message);
-        });
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.photoService
+      .listByUser('flavio')
+      .subscribe(
+        photos => this.photos = photos,
+        err => {throw new Error(err.message);
+      });
   }
 
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Photo } from './photo';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-    constructor(private http: HttpClient) {
-       this.http = http;
-    }
+    constructor(private http: HttpClient) { }
 
     listByUser(name: string) {
-        return this.http.get<Response[]>(`http://localhost:3000/${name}/photos`);
+        return this.http.get<Photo[]>(`http://localhost:3000/${name}/photos`);
     }
 
 }
