d5f6662e1190b83d2348fb6593488abc9de434f7
Gabriel Conceicao <gabrielconceicao.dev@gmail.com>
2020-05-31 17:04:58 -0300

    Define Routes to my project and improve the code

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Photo } from './photo';
 
+const path = 'http://localhost:3000/';
+
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
     constructor(private http: HttpClient) { }
 
     listByUser(name: string) {
-        return this.http.get<Photo[]>(`http://localhost:3000/${name}/photos`);
+        return this.http.get<Photo[]>(`${path}${name}/photos`);
     }
 
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
-import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { AppRoutingModule } from '../app.routing.module';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+
 
 @NgModule({
-    declarations : [ PhotoComponent ],
-    exports : [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations : [
+        PhotoComponent,
+        PhotoListComponent,
+        PhotoFormComponent,
+        PhotosComponent
+    ],
+    exports : [
+        AppRoutingModule
+    ],
+    imports: [
+        HttpClientModule,
+        CommonModule,
+    ]
 })
 export class PhotosModule {
 
