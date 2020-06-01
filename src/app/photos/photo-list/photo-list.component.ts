72eb929df193dac6cb67b6591811eb013bb12570
Gabriel Conceicao <gabrielconceicao.dev@gmail.com>
2020-05-31 21:24:27 -0300

    Search bar implemented and using debounce from Rxjs

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
+  filter = '';
+  debounce: Subject<string> = new Subject<string>();
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.photos = this.activatedRoute.snapshot.data.photos;
 
-    const userName = this.activatedRoute.snapshot.params.userName;
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
 
-    this.photoService
-      .listByUser(userName)
-      .subscribe(
-        photos => this.photos = photos,
-        err => {throw new Error(err.message);
-      });
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
